ae47be0f8990e6fd2c7ebb37a7f8fd482a1af2a5
ShawnTseng <shawntseng40@gmail.com>
2019-03-05 10:06:22 +0800

    boolean experiment

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('demo1').innerHTML = Boolean("False");
+document.getElementById('demo2').innerHTML = Boolean(0);
+document.getElementById('demo3').innerHTML = Boolean(-0);
+document.getElementById('demo4').innerHTML = Boolean("");
+document.getElementById('demo5').innerHTML = Boolean(null);
+document.getElementById('demo6').innerHTML = Boolean(false);
+document.getElementById('demo7').innerHTML = Boolean(NaN);
