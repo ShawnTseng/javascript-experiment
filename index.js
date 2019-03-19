fd15cf8db8b00a707d0f11d174f67f89a32e3e79
ShawnTseng <shawntseng40@gmail.com>
2019-03-19 14:25:44 +0800

    let experiment

---
+++ index.js
@@ -0,0 +1,15 @@
+var name = 'Ray';
+document.getElementById('demo1').innerHTML = name;
+{
+  var name = 'Shawn';
+  document.getElementById('demo2').innerHTML = name;
+}
+document.getElementById('demo3').innerHTML = name;
+
+var dept = 'im';
+document.getElementById('demo4').innerHTML = dept;
+{
+  let dept = 'mkpl';
+  document.getElementById('demo5').innerHTML = dept;
+}
+document.getElementById('demo6').innerHTML = dept;
