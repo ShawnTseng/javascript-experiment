e534963f81c313260883c276bd04ed7642e64ecf
ShawnTseng <shawntseng40@gmail.com>
2019-05-07 13:09:09 +0800

    clear

--- index.js
+++
@@ -1,6 +0,0 @@
-const nodeList = document.querySelectorAll('p');
-console.log(nodeList);
-
-document.getElementById('demo1').textContent = nodeList[1].textContent;
-
-document.getElementById('demo2').textContent = nodeList.length;
