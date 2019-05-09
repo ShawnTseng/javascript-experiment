f0ed0140fde19da6bccf715cdd8bcc2e48ee208e
ShawnTseng <shawntseng40@gmail.com>
2019-05-09 13:16:50 +0800

    clear

--- index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('demo1').textContent = screen.width;
-document.getElementById('demo2').textContent = screen.height;
-document.getElementById('demo3').textContent = screen.availWidth;
-document.getElementById('demo4').textContent = screen.availHeight;
-document.getElementById('demo5').textContent = screen.colorDepth;
-document.getElementById('demo6').textContent = screen.pixelDepth;
