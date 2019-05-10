6a1dec79ab4612a6b3f86ca6699cb3ced73a2d44
ShawnTseng <shawntseng40@gmail.com>
2019-05-10 13:24:18 +0800

    clear

--- index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('demo1').textContent = location.href;
-document.getElementById('demo2').textContent = location.hostname;
-document.getElementById('demo3').textContent = location.pathname;
-document.getElementById('demo4').textContent = location.protocol;
-document.getElementById('demo5').textContent = location.port;
-
-function forwardToMyPage() {
-    location.assign("https://shawntseng.blogspot.com/");
-}
